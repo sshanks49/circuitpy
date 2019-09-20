c6e457a998d33fde5b5b72d5ff70ddd48640e111
Seanan E Shanks <sshanks49@ccs.local>
2019-09-20 12:03:26 -0400

    Update two_fancy

--- two_fancy.py
+++ two_fancy.py
@@ -1,2 +1,10 @@
-from board import *
-from time import sleep
+from fancyLED import FancyLED
+
+fancy1 = FancyLED(2,3,4)
+fancy2 = FancyLED(5,6,7)
+
+while True:
+    fancy1.alternate()
+    fancy2.blink()
+    fancy1.chase()
+    fancy2.sparkle()
