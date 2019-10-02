302b1473e0f96d24e4c8a2bdcfce089d444075c4
Seanan E Shanks <sshanks49@ccs.local>
2019-10-02 10:54:37 -0400

    Update lcdbutton.py

--- lcdbutton.py
+++ lcdbutton.py
@@ -12,18 +12,29 @@ switch = DigitalInOut(board.D3)
 switch.direction = Direction.INPUT
 switch.pull = Pull.UP
 
+button = DigitalInOut(board.D4)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+
 pressed = True
 lastState = True
 count = 0
+switched = False
+lastswitch = False
+dirn = 1
 
 while True:
     lcd.set_cursor_pos(0, 0)
-    lcd.print("Counting up")
+    lcd.print("Counting ",("down","up")[dirn==1])
     pressed = switch.value
+    switched = button.value
     if not pressed and lastState:
-        count += 1
+        count += dirn
+    if not switched and lastswtich:
+        dirn = -dirn
     lcd.set_cursor_pos(1, 0)
     lcd.print("Presses:        ")
     lcd.set_cursor_pos(1,8)
     lcd.print(str(count))
     lastState = pressed
+    lastswitch = switched
